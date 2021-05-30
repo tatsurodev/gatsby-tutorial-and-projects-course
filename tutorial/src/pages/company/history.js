5771cab9313c3334caeb5478e0c156f0250b007d
tatsurodev <contacts@tatsuro.dev>
2021-05-30 18:50:33 +0900

    Link使用 14

--- tutorial/src/pages/company/history.js
+++ tutorial/src/pages/company/history.js
@@ -1,7 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const history = () => {
-  return <div>this is history of our company</div>
+  return (
+    <div>
+      <h2>this is history of our company</h2>
+      <Link to="/">home page</Link>
+    </div>
+  )
 }
 
 export default history
--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <h1>hello people</h1>
+  return (
+    <div>
+      <h1>hello people</h1>
+      <div>
+        <Link to="/about">about</Link>
+      </div>
+      <div>
+        <Link to="/company/history">history</Link>
+      </div>
+      <a href="/about">reg link</a>
+      <a href="https://www.gatsbyjs.com/docs/how-to">gatsby docs</a>
+    </div>
+  )
 }
