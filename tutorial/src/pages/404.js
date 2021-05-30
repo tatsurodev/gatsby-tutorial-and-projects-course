507478498aff27b512dfca7de2823cb8a2bef05b
tatsurodev <contacts@tatsuro.dev>
2021-05-30 15:41:09 +0900

    error page作成 12

---
+++ tutorial/src/pages/404.js
@@ -0,0 +1,9 @@
+// rafce
+import React from "react"
+
+// 404 pageはfile名が404.jsであることが必須だが、component名が数字だとreactでerrorが出るので変更
+const Error = () => {
+  return <div>Error Page</div>
+}
+
+export default Error
