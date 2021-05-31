4acfe2c8df7a9908e5c0933201a83b83c5136d0d
tatsurodev <contacts@tatsuro.dev>
2021-05-31 18:32:56 +0900

    css moduleを使用して名前衝突回避 21

--- tutorial/src/pages/about.js
+++ tutorial/src/pages/about.js
@@ -1,12 +1,23 @@
 // rafce
 import React from "react"
 import Layout from "../components/Layout"
+import * as aboutStyles from "../examples/about.module.css"
 
 const about = () => {
+  // css fileに.module.cssの拡張子を付与することでcss moduleとしてgatsbyに認識され、class名にrandomなclass名が付与されるので名前衝突を回避できる
+  // console.log(aboutStyles)
+
   return (
     <Layout>
-      <div className="about-heading">
+      <div className={aboutStyles.page}>
         <h1>about page</h1>
+        <h1>hello world</h1>
+        <p className={aboutStyles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+          adipisci illum, consequatur quos, dolore qui sint earum tempore, rerum
+          similique autem error! Illo eveniet libero cupiditate quis fugiat,
+          odit cum!
+        </p>
       </div>
     </Layout>
   )
--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -1,15 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { page, text } from "../examples/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <div className="home-heading">
+      <div className={page}>
         {/* global cssよりinline cssの方が強い */}
         <h1>home page</h1>
         <h1>hello world</h1>
         <h1>hello people</h1>
+        <p className={text}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+          animi suscipit, sapiente, beatae velit ea quis assumenda excepturi
+          blanditiis voluptatibus impedit maxime ratione sit sunt, accusamus
+          voluptate debitis aperiam illo!
+        </p>
       </div>
     </Layout>
   )
