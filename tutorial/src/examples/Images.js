be563a226d7094b0c4e1a487c947084673d991b9
tatsurodev <contacts@tatsuro.dev>
2021-06-02 06:49:48 +0900

    StaticImageのattribute例 36

--- tutorial/src/examples/Images.js
+++ tutorial/src/examples/Images.js
@@ -1,25 +1,35 @@
 import React from "react"
 import styled from "styled-components"
-import big from "../assets/images/big.jpg"
 import { StaticImage } from "gatsby-plugin-image"
 
 const Images = () => {
   return (
     <Wrapper>
-      {/* StaticImageのattributeは外部に依存できない、src={props.src}とかはダメ */}
-      <StaticImage src="../assets/images/big.jpg" alt="food" />
-      <StaticImage
-        src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1613583026/course%20slides/js-2_mo9uqy.png"
-        alt="code"
-      />
-      <h2>gatsby image</h2>
+      <article>
+        <h4>constrained / default</h4>
+        {/* propsに関しては https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-plugin-image/ */}
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          height={400}
+          placeholder="tracedSVG"
+          layout="constrained"
+          className="example-img"
+          as="section"
+        />
+      </article>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
-  img {
-    width: 200px;
+  width: 70vw;
+  margin: 0 auto;
+  article {
+    border: 2px solid red;
+  }
+  .example-img {
+    border-radius: 1rem;
   }
 `
 
