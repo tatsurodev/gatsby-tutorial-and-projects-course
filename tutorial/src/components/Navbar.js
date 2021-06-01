2b2a8efb869de5499002fa8d27bb8c335bb0fd44
tatsurodev <contacts@tatsuro.dev>
2021-06-01 13:26:58 +0900

    react-icons install, asset用意 28

--- tutorial/src/components/Navbar.js
+++ tutorial/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
+import { FiAlignJustify } from "react-icons/fi"
 
 const Navbar = () => {
   return (
     <nav>
+      <FiAlignJustify />
       <ul>
         <li>
           <Link to="/">home</Link>
