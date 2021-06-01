72a38bf509aea8f9691d638da04f581fef9f1fff
tatsurodev <contacts@tatsuro.dev>
2021-06-01 17:43:25 +0900

    StaticImage使用 34

--- tutorial/src/examples/Images.js
+++ tutorial/src/examples/Images.js
@@ -1,11 +1,17 @@
 import React from "react"
 import styled from "styled-components"
 import big from "../assets/images/big.jpg"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Images = () => {
   return (
     <Wrapper>
-      <img src={big} alt="food" />
+      {/* StaticImageのattributeは外部に依存できない、src={props.src}とかはダメ */}
+      <StaticImage src="../assets/images/big.jpg" alt="food" />
+      <StaticImage
+        src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1613583026/course%20slides/js-2_mo9uqy.png"
+        alt="code"
+      />
       <h2>gatsby image</h2>
     </Wrapper>
   )
