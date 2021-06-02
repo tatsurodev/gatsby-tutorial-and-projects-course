33155b6b03ddcd241b8e10c01d0c8690e31308e2
tatsurodev <contacts@tatsuro.dev>
2021-06-02 10:16:09 +0900

    About作成 39

--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -1,13 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import Images from "../examples/Images"
 
 export default function Home() {
   return (
     <Layout>
       <h1>home page</h1>
-      <Images />
     </Layout>
   )
 }
