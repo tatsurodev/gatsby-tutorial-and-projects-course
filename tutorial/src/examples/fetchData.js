dcf132a006d91d0e37abf31ce72ad86209305958
tatsurodev <contacts@tatsuro.dev>
2021-06-03 19:44:02 +0900

    react hooksでuseStaticQueryを使用 49

--- tutorial/src/examples/fetchData.js
+++ tutorial/src/examples/fetchData.js
@@ -1,41 +1,50 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-const ComponentName = () => {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          author
-          description
-          simpleData
-          title
-          complexData {
-            age
-            name
-          }
-          person {
-            age
-            name
-          }
+/* graphql``で返ってくるものは、下記のdata propertyの値部分のobject
+{
+  "data": {
+    ...
+  }
+}
+*/
+const getData = graphql`
+  {
+    site {
+      siteMetadata {
+        author
+        description
+        simpleData
+        title
+        complexData {
+          age
+          name
+        }
+        person {
+          age
+          name
         }
       }
     }
-  `)
+  }
+`
+
+// react hooksのcomponent名は、uppercaseであることが必須
+const FetchData = () => {
+  // useStaticQueryはcomponent内で実行しないとerror
+  // const data = useStaticQuery(getData)
+  const {
+    site: {
+      siteMetadata: { title },
+    },
+  } = useStaticQuery(getData)
+
   return (
     <div>
-      <h2>{data.site.siteMetadata.person.name}</h2>
-      <div>
-        {data.site.siteMetadata.complexData.map((item, index) => {
-          return (
-            <p key={index}>
-              {item.name}:{item.age}
-            </p>
-          )
-        })}
-      </div>
+      {/* <h2>Name : {data.site.siteMetadata.person.name}</h2> */}
+      <h2>site title is : {title}</h2>
     </div>
   )
 }
 
-export default ComponentName
+export default FetchData
