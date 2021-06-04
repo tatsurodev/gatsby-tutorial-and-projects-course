d7a1db241ae852c622d432c2964c9aff91a59f77
tatsurodev <contacts@tatsuro.dev>
2021-06-04 22:04:52 +0900

    Galleryの準備 59

---
+++ tutorial/src/examples/Gallery.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Gallery = () => {
+  return (
+    <div>
+      <h2>simple image gallery</h2>
+    </div>
+  )
+}
+
+export default Gallery
--- tutorial/src/pages/testing.js
+++ tutorial/src/pages/testing.js
@@ -1,34 +1,16 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import Gallery from "../examples/Gallery"
 
-const Testing = ({ data }) => {
-  const author = data.site.info.author
+const Testing = () => {
   return (
-    <div>
-      <h2>author : {author}</h2>
-    </div>
+    <Layout>
+      <main className="page">
+        <Gallery />
+      </main>
+    </Layout>
   )
 }
 
-// page componentでqueryがexportされると、propsのdata propertyとしてquery結果がsetされる。data以外の変数名を指定してもprops.dataでaccessすることになるので変数名は何でもおｋ
-export const data = graphql`
-  {
-    site {
-      info: siteMetadata {
-        author
-        description
-        simpleData
-        title
-        complexData {
-          age
-          name
-        }
-        person {
-          age
-          name
-        }
-      }
-    }
-  }
-`
 export default Testing
