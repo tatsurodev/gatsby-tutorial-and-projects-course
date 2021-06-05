18fd1883e7fa860cbf077dd46c3223b7d8c483aa
tatsurodev <contacts@tatsuro.dev>
2021-06-06 03:26:09 +0900

    AllRecipes作成 72

---
+++ tutorial/src/components/AllRecipes.js
@@ -0,0 +1,39 @@
+import React from "react"
+import TagsList from "./TagsList"
+import RecipesList from "./RecipesList"
+import { graphql, useStaticQuery } from "gatsby"
+
+const query = graphql`
+  {
+    allContentfulRecipe(sort: { fields: title, order: ASC }) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        content {
+          tags
+        }
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`
+
+const AllRecipes = () => {
+  const data = useStaticQuery(query)
+  const recipes = data.allContentfulRecipe.nodes
+  // console.log(recipes)
+
+  return (
+    <div>
+      <h4>all recipes</h4>
+      <TagsList recipes={recipes} />
+      <RecipesList recipes={recipes} />
+    </div>
+  )
+}
+
+export default AllRecipes
