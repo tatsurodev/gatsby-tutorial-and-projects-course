c26e6f09fae6dcc29d932d1f69be40163b5421d4
tatsurodev <contacts@tatsuro.dev>
2021-06-08 06:14:34 +0900

    query variableの使用 82

--- tutorial/src/pages/{ContentfulRecipe.title}.js
+++ tutorial/src/pages/{ContentfulRecipe.title}.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 const RecipeTemplate = props => {
   return (
@@ -8,4 +9,13 @@ const RecipeTemplate = props => {
   )
 }
 
+export const query = graphql`
+  query getSingleRecipe($title: String) {
+    contentfulRecipe(title: { eq: $title }) {
+      title
+      cookTime
+    }
+  }
+`
+// console.log("title", $title)
 export default RecipeTemplate
