6e4c9874c1c46448c7e241b2ec96f70c5073d620
tatsurodev <contacts@tatsuro.dev>
2021-06-09 03:56:03 +0900

    fontのpreload 89

--- tutorial/gatsby-config.js
+++ tutorial/gatsby-config.js
@@ -40,5 +40,22 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_API_KEY,
       },
     },
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: "Montserrat",
+              variants: ["300", "400", "500"],
+            },
+            {
+              family: "Inconsolata",
+              variants: ["400", "500", "600", "700"],
+            },
+          ],
+        },
+      },
+    },
   ],
 }
