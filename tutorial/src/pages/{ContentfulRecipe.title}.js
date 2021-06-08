e5229c696ed5a0cea1571da12790be3e67fb57c7
tatsurodev <contacts@tatsuro.dev>
2021-06-08 19:39:37 +0900

    Recipe templateの表示完成 84

--- tutorial/src/pages/{ContentfulRecipe.title}.js
+++ tutorial/src/pages/{ContentfulRecipe.title}.js
@@ -1,8 +1,10 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { BsClockHistory, BsClock, BsPeople } from "react-icons/bs"
+import Layout from "../components/Layout"
 
+// ファイル名で指定したfiled(ここではtitle)は、component内ではprops.params.titleで、graphql内ではvariable(ここでは$title)でaccessできる
 const RecipeTemplate = ({ data }) => {
   const {
     title,
@@ -17,11 +19,92 @@ const RecipeTemplate = ({ data }) => {
   const { tags, instructions, ingredients, tools } = content
 
   return (
-    <main className="page">
-      <div className="recipe-page">
-        <h2>{title}</h2>
-      </div>
-    </main>
+    <Layout>
+      <main className="page">
+        <div className="recipe-page">
+          {/* hero */}
+          <section className="recipe-hero">
+            <GatsbyImage
+              image={pathToImage}
+              alt={title}
+              className="about-img"
+            />
+            <article className="recipe-info">
+              <h2>{title}</h2>
+              <p>{description}</p>
+              {/* icons */}
+              <div className="recipe-icons">
+                <article>
+                  <BsClock />
+                  <h5>prep time</h5>
+                  <p>{prepTime} min.</p>
+                </article>
+                <article>
+                  <BsClockHistory />
+                  <h5>cook time</h5>
+                  <p>{cookTime} min.</p>
+                </article>
+                <article>
+                  <BsPeople />
+                  <h5>serving</h5>
+                  <p>{servings}</p>
+                </article>
+              </div>
+              {/* tags */}
+              <p className="recipe-tags">
+                Tags :
+                {tags.map((tag, index) => {
+                  return (
+                    <Link to={`/${tag}`} key={index}>
+                      {tag}
+                    </Link>
+                  )
+                })}
+              </p>
+            </article>
+          </section>
+          {/* rest of the content */}
+          <section className="recipe-content">
+            <article>
+              <h4>instructions</h4>
+              {instructions.map((item, index) => {
+                return (
+                  <div key={index} className="single-instruction">
+                    <header>
+                      <p>step {index + 1}</p>
+                      <div></div>
+                    </header>
+                    <p>{item}</p>
+                  </div>
+                )
+              })}
+            </article>
+            <article className="second-column">
+              <div>
+                <h4>ingredients</h4>
+                {ingredients.map((item, index) => {
+                  return (
+                    <p key={index} className="single-ingredient">
+                      {item}
+                    </p>
+                  )
+                })}
+              </div>
+              <div>
+                <h4>tools</h4>
+                {tools.map((item, index) => {
+                  return (
+                    <p key={index} className="single-tool">
+                      {item}
+                    </p>
+                  )
+                })}
+              </div>
+            </article>
+          </section>
+        </div>
+      </main>
+    </Layout>
   )
 }
 
