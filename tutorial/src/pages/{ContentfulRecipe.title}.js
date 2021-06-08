92ee3c93a42ca8afadf7f392e3ccee3720c6f3a3
tatsurodev <contacts@tatsuro.dev>
2021-06-08 18:31:41 +0900

    Recipe templateで必要なfieldsを取得 83

--- tutorial/src/pages/{ContentfulRecipe.title}.js
+++ tutorial/src/pages/{ContentfulRecipe.title}.js
@@ -1,11 +1,27 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { BsClockHistory, BsClock, BsPeople } from "react-icons/bs"
 
-const RecipeTemplate = props => {
+const RecipeTemplate = ({ data }) => {
+  const {
+    title,
+    cookTime,
+    content,
+    prepTime,
+    servings,
+    description: { description },
+    image,
+  } = data.contentfulRecipe
+  const pathToImage = getImage(image)
+  const { tags, instructions, ingredients, tools } = content
+
   return (
-    <div>
-      <h2>{props.params.title}</h2>
-    </div>
+    <main className="page">
+      <div className="recipe-page">
+        <h2>{title}</h2>
+      </div>
+    </main>
   )
 }
 
@@ -14,6 +30,20 @@ export const query = graphql`
     contentfulRecipe(title: { eq: $title }) {
       title
       cookTime
+      content {
+        ingredients
+        instructions
+        tags
+        tools
+      }
+      description {
+        description
+      }
+      prepTime
+      servings
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
     }
   }
 `
