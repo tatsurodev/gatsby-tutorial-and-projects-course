64f582d484d766fd3e9324746b66bd5a8bc08ebc
tatsurodev <contacts@tatsuro.dev>
2021-06-14 08:40:21 +0900

    gatsbyをstrapiへ接続 111

--- portfolio/frontend/gatsby-config.js
+++ portfolio/frontend/gatsby-config.js
@@ -19,5 +19,14 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Defaults to 100
+        collectionTypes: [`job`],
+        // singleTypes: [`home-page`, `contact`],
+      },
+    },
   ],
 }
