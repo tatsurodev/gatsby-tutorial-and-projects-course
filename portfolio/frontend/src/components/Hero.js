5839d641188186ad40beb6c6506225747982cffc
tatsurodev <contacts@tatsuro.dev>
2021-06-14 03:11:51 +0900

    Hero作成 105

--- portfolio/frontend/src/components/Hero.js
+++ portfolio/frontend/src/components/Hero.js
@@ -5,7 +5,39 @@ import socialLinks from "../constants/social_links"
 import { StaticImage } from "gatsby-plugin-image"
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  return (
+    <header className="hero">
+      <section className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>i'm john</h1>
+            <h4>freelance web and mobile UI/UX Designer</h4>
+            <Link to="/contact" className="btn">
+              contact me
+            </Link>
+            <div className="social-links">
+              {socialLinks.map(link => {
+                return (
+                  <a href={link.url} key={link.id} className="social-link">
+                    {link.icon}
+                  </a>
+                )
+              })}
+            </div>
+          </div>
+        </article>
+        {/* StaticImageをsvgで使用する時の注意点 小さいheightを使用すると切れるのでwidthで調整すること。もしくは、普通にimgを使ってheightを指定する */}
+        <StaticImage
+          src="../assets/images/hero.svg"
+          alt="portfolio"
+          className="hero-img"
+          placeholder="blurred"
+        />
+        {/* <img src={heroImg} alt="portfolio" className="hero-img-svg" /> */}
+      </section>
+    </header>
+  )
 }
 
 export default Hero
