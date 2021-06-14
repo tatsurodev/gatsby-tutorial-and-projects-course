a9c75ecb2bf82d1e06f3e5df6596e468e5a3f668
tatsurodev <contacts@tatsuro.dev>
2021-06-14 11:17:13 +0900

    projectを取得するquery 119

--- portfolio/frontend/src/pages/index.js
+++ portfolio/frontend/src/pages/index.js
@@ -6,7 +6,9 @@ import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Seo from "../components/Seo"
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  console.log(data)
+
   return (
     <>
       <main>
@@ -17,5 +19,33 @@ const IndexPage = () => {
     </>
   )
 }
+
+export const query = graphql`
+  {
+    allStrapiProject(filter: { featured: { eq: true } }) {
+      nodes {
+        description
+        featured
+        github
+        id
+        slug
+        title
+        url
+        stack {
+          id
+          title
+        }
+        image {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+            }
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`
 
 export default IndexPage
