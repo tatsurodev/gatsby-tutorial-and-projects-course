15cf8a7774a8cafb737ec94c3e7b2ef1b1c97b99
tatsurodev <contacts@tatsuro.dev>
2021-06-18 10:07:22 +0900

    project templateのquery作成 124

--- portfolio/frontend/src/pages/projects/{StrapiProject.title}.js
+++ portfolio/frontend/src/pages/projects/{StrapiProject.title}.js
@@ -1,17 +1,36 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Seo from "../../components/Seo"
 
 // pageContext vs params
 // pageContextはqueryで取得したfield(ここではtitle)に、paramsではurlに関連する値にaccessできる
-const ProjectTemplate = ({ pageContext, params }) => {
+const ProjectTemplate = ({ pageContext, params, data }) => {
   const { title } = pageContext
   // console.log("pageContext", pageContext)
   // console.log("params", params)
 
   return (
-    <div>
-      <h4>{title}</h4>
-    </div>
+    <>
+      <main className="project-template-page">
+        <h2>{title}</h2>
+        <p>{data.strapiProject.description}</p>
+      </main>
+    </>
   )
 }
+
+export const query = graphql`
+  query GetSingleProject($title: String) {
+    strapiProject(title: { eq: $title }) {
+      description
+      title
+      image {
+        localFile {
+          publicURL
+        }
+      }
+    }
+  }
+`
 
 export default ProjectTemplate
