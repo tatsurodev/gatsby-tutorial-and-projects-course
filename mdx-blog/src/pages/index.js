9a96b6dc4ee752182b4190e0e1e8777782a0b2e6
Tatsuro <contacts@tatsuro.dev>
2021-10-05 22:12:40 +0900

    indexで表示するpostsのquery作成 163

--- mdx-blog/src/pages/index.js
+++ mdx-blog/src/pages/index.js
@@ -3,12 +3,37 @@ import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  console.log(data)
   return (
     <Layout>
       <Hero showPerson />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allMdx(limit: 3, sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          slug
+          date(formatString: "MMM, Do YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`
 
 export default IndexPage
