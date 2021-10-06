24adc8a1f36bf0b47ffaf7067c201e76473892ae
Tatsuro <contacts@tatsuro.dev>
2021-10-06 15:43:41 +0900

    category取得用のquery作成 171

--- mdx-blog/src/templates/category-template.js
+++ mdx-blog/src/templates/category-template.js
@@ -4,14 +4,36 @@ import Hero from '../components/Hero'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
 
-const CategoryTemplate = ()=> {
-  
-  return (
-   <h2>category template</h2>
-  )
+const CategoryTemplate = ({ data }) => {
+  console.log(data)
+  return <h2>category template</h2>
 }
 
-
-
+export const query = graphql`
+  query GetCategories($category: String) {
+    allMdx(
+      sort: { fields: frontmatter___date, order: DESC }
+      filter: { frontmatter: { category: { eq: $category } } }
+    ) {
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          slug
+          date(formatString: "MMMM Do, YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`
 
 export default CategoryTemplate
