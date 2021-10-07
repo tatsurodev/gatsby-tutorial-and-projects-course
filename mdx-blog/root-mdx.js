f2fd0d53353e300fc1deba31af91ce20cce0d1f8
Tatsuro <contacts@tatsuro.dev>
2021-10-07 19:31:32 +0900

    code block用componentにprismを使用 190

--- mdx-blog/root-mdx.js
+++ mdx-blog/root-mdx.js
@@ -1,7 +1,7 @@
 // gatsby-browser.js, gatsby-ssr.jsの両方に記述するcodeは、変更の際両方のcodeを変更しないといけなくなるのでまとめておく
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import { Headings, Code, Blockquote } from './src/components/Complete'
+import { Headings, Code, Blockquote, Example } from './src/components/Complete'
 
 // https://mdxjs.com/table-of-components/
 const components = {
@@ -9,6 +9,7 @@ const components = {
   h4: Headings.myH4,
   inlineCode: Code,
   blockquote: Blockquote,
+  pre: Example,
 }
 
 export const wrapMDX = ({ element }) => {
