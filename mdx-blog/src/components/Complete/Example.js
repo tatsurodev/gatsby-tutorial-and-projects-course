092908de1d11edd9af10fcb4a8eed1a162ff5cc7
Tatsuro <contacts@tatsuro.dev>
2021-10-07 19:55:39 +0900

    prismのconfigでcode, language, theme指定 191

--- mdx-blog/src/components/Complete/Example.js
+++ mdx-blog/src/components/Complete/Example.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
-import theme from 'prism-react-renderer/themes/vsDark'
+import theme from 'prism-react-renderer/themes/github'
 
 const exampleCode = `
 (function someDemo() {
@@ -9,9 +9,22 @@ const exampleCode = `
 })()
 `
 
-const Example = () => {
+const Example = props => {
+  // console.log(props)
+  // props.children.props以下にcode blockの情報がある
+  // ex. className: ```jsxならlanguage-jsxが得られる
+  const className = props.children.props.className
+  const language = className.replace(/language-/, '')
   return (
-    <Highlight {...defaultProps} code={exampleCode} language="jsx">
+    <Highlight
+      {...defaultProps}
+      // code blockの中身
+      code={props.children.props.children.trim()}
+      // ```languageで指定した言語の種類
+      language={language}
+      // 使用するtheme
+      theme={theme}
+    >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre className={className} style={style}>
           {tokens.map((line, i) => (
