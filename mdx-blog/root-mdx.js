6ce1466327ee0906702ea02b45dcb1f0b78be4b0
Tatsuro <contacts@tatsuro.dev>
2021-10-07 18:27:41 +0900

    h2用component作成 186

--- mdx-blog/root-mdx.js
+++ mdx-blog/root-mdx.js
@@ -1,24 +1,10 @@
 // gatsby-browser.js, gatsby-ssr.jsの両方に記述するcodeは、変更の際両方のcodeを変更しないといけなくなるのでまとめておく
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-
-const myH2 = props => {
-  if (props.title) {
-    return (
-      <h2 {...props} style={{ fontSize: '2rem', color: 'red' }}>
-        {props.children}
-      </h2>
-    )
-  }
-  return (
-    <h2 {...props} className="code">
-      {props.children}
-    </h2>
-  )
-}
+import { Headings } from './src/components/Complete'
 
 const components = {
-  h2: myH2,
+  h2: Headings.myH2,
 }
 
 export const wrapMDX = ({ element }) => {
