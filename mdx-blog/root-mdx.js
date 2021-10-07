751bc6f301481b630dc8080ab6734c776b637b59
Tatsuro <contacts@tatsuro.dev>
2021-10-07 19:12:19 +0900

    blockquote用component作成 189

--- mdx-blog/root-mdx.js
+++ mdx-blog/root-mdx.js
@@ -1,17 +1,14 @@
 // gatsby-browser.js, gatsby-ssr.jsの両方に記述するcodeは、変更の際両方のcodeを変更しないといけなくなるのでまとめておく
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import { Headings, Code } from './src/components/Complete'
-
-const Testing = ({ children }) => {
-  return <code className="code">{children}</code>
-}
+import { Headings, Code, Blockquote } from './src/components/Complete'
 
 // https://mdxjs.com/table-of-components/
 const components = {
   h2: Headings.myH2,
   h4: Headings.myH4,
   inlineCode: Code,
+  blockquote: Blockquote,
 }
 
 export const wrapMDX = ({ element }) => {
